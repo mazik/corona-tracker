16cecf3ae42331bdeb6b023dcb5a268b9767d3d4
Md Alauddin Al Amin <shakeel.techlogicians@gmail.com>
2020-04-09 01:06:58 +0600

    Extend transition property , custom space & insets for enhancement

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,87 +1,108 @@
 module.exports = {
-  theme: {
-    animations: { // defaults to {}; the following are examples
-      'spin': {
-        from: {
-          transform: 'rotate(0deg)',
+    theme: {
+        animations: { // defaults to {}; the following are examples
+            'spin': {
+                from: {
+                    transform: 'rotate(0deg)',
+                },
+                to: {
+                    transform: 'rotate(360deg)',
+                },
+            },
+            'jump': {
+                '0%': {
+                    transform: 'translateY(0%)',
+                },
+                '50%': {
+                    transform: 'translateY(-100%)',
+                },
+                '100%': {
+                    transform: 'translateY(0%)',
+                },
+            },
         },
-        to: {
-          transform: 'rotate(360deg)',
+        animationDuration: { // defaults to these values
+            'default': '1s',
+            '0s': '0s',
+            '1s': '1s',
+            '2s': '2s',
+            '3s': '3s',
+            '4s': '4s',
+            '5s': '5s',
         },
-      },
-      'jump': {
-        '0%': {
-          transform: 'translateY(0%)',
+        animationTimingFunction: { // defaults to these values
+            'default': 'ease',
+            'linear': 'linear',
+            'ease': 'ease',
+            'ease-in': 'ease-in',
+            'ease-out': 'ease-out',
+            'ease-in-out': 'ease-in-out',
         },
-        '50%': {
-          transform: 'translateY(-100%)',
+        animationDelay: { // defaults to these values
+            'default': '0s',
+            '0s': '0s',
+            '1s': '1s',
+            '2s': '2s',
+            '3s': '3s',
+            '4s': '4s',
+            '5s': '5s',
         },
-        '100%': {
-          transform: 'translateY(0%)',
+        animationIterationCount: { // defaults to these values
+            'default': 'infinite',
+            'once': '1',
+            'infinite': 'infinite',
         },
-      },
+        animationDirection: { // defaults to these values
+            'default': 'normal',
+            'normal': 'normal',
+            'reverse': 'reverse',
+            'alternate': 'alternate',
+            'alternate-reverse': 'alternate-reverse',
+        },
+        animationFillMode: { // defaults to these values
+            'default': 'none',
+            'none': 'none',
+            'forwards': 'forwards',
+            'backwards': 'backwards',
+            'both': 'both',
+        },
+        animationPlayState: { // defaults to these values
+            'running': 'running',
+            'paused': 'paused',
+        },
+        extend: {
+            transitionProperty: { // extending few more transition property
+                'height': 'height',
+                'spacing': 'margin, padding',
+                'slide-left' : 'margin-right'
+            },
+            spacing: {
+                '0px' : '0px',
+                '5px': '5px',
+                '12px': '12px',
+                '393px' : '393px',
+                '-200px' : '-200px',
+                '200px': '200px',
+                '21px': '21px',
+            }
+        },
+        inset: {
+            '0': 0,
+            '3px' : '3px',
+            '-25px' : '-25px'
+        }
     },
-    animationDuration: { // defaults to these values
-      'default': '1s',
-      '0s': '0s',
-      '1s': '1s',
-      '2s': '2s',
-      '3s': '3s',
-      '4s': '4s',
-      '5s': '5s',
+    variants: { // all the following default to ['responsive']
+        animations: ['responsive'],
+        animationDuration: ['responsive'],
+        animationTimingFunction: ['responsive'],
+        animationDelay: ['responsive'],
+        animationIterationCount: ['responsive'],
+        animationDirection: ['responsive'],
+        animationFillMode: ['responsive'],
+        animationPlayState: ['responsive'],
     },
-    animationTimingFunction: { // defaults to these values
-      'default': 'ease',
-      'linear': 'linear',
-      'ease': 'ease',
-      'ease-in': 'ease-in',
-      'ease-out': 'ease-out',
-      'ease-in-out': 'ease-in-out',
-    },
-    animationDelay: { // defaults to these values
-      'default': '0s',
-      '0s': '0s',
-      '1s': '1s',
-      '2s': '2s',
-      '3s': '3s',
-      '4s': '4s',
-      '5s': '5s',
-    },
-    animationIterationCount: { // defaults to these values
-      'default': 'infinite',
-      'once': '1',
-      'infinite': 'infinite',
-    },
-    animationDirection: { // defaults to these values
-      'default': 'normal',
-      'normal': 'normal',
-      'reverse': 'reverse',
-      'alternate': 'alternate',
-      'alternate-reverse': 'alternate-reverse',
-    },
-    animationFillMode: { // defaults to these values
-      'default': 'none',
-      'none': 'none',
-      'forwards': 'forwards',
-      'backwards': 'backwards',
-      'both': 'both',
-    },
-    animationPlayState: { // defaults to these values
-      'running': 'running',
-      'paused': 'paused',
-    },
-  },
-  variants: { // all the following default to ['responsive']
-    animations: ['responsive'],
-    animationDuration: ['responsive'],
-    animationTimingFunction: ['responsive'],
-    animationDelay: ['responsive'],
-    animationIterationCount: ['responsive'],
-    animationDirection: ['responsive'],
-    animationFillMode: ['responsive'],
-    animationPlayState: ['responsive'],
-  },
-  plugins: [
-    require('tailwindcss-animations'),
-  ],
+    plugins: [
+        require('tailwindcss-animations'),
+    ],
 };
